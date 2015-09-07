255fd86d9d17811e8b272d755cdbecdb436576c2
Noah Thomas <freerunner33@github.com>
2015-09-07 11:17:27 -0700

    Changed to schedule app

---
+++ js/app.js
@@ -0,0 +1,368 @@
+var schedule = [
+	[ // Sunday
+		{
+			heading: 'Breakfast',
+			elements:['Scrambled eggs', 'Apple with peanut butter']
+		},
+		{
+			heading: 'Homework',
+			elements:['9:00 - 11:00', 'EVL and water around 11:30']
+		},
+		{
+			heading: 'Beach or Hockey',
+			elements:['12:00 - 2:00']
+		},
+		{
+			heading: 'Lunch',
+			elements:['Steak, carrots, cucumbers']
+		},
+		{
+			heading: 'Homework',
+			elements:['3:00 - 4:30']
+		},
+		{
+			heading: 'Snack',
+			elements:['Carrots, cucumbers, yogurt']
+		},
+		{
+			heading: 'Homework',
+			elements:['5:00 - 6:30', 'EVL and water around 6:00']
+		},
+		{
+			heading: 'Cardio',
+			elements:['Run in valley', 'Take left path']
+		},
+		{
+			heading: 'Workout',
+			elements:['Manchester field', 'Core on bars']
+		},
+		{
+			heading: 'Post workout',
+			elements:['Water and shower']
+		},
+		{
+			heading: 'Dinner',
+			elements:['Large salad', 'Tuna and 2 eggs']
+		},
+		{
+			heading: 'Mass',
+			elements:['Founders Chapel at 9:00pm']
+		},
+		{
+			heading: 'Sleep',
+			elements:['Go to sleep by 11:00', 'Wake up at 8:00']
+		}
+	],
+	[	// Monday
+		{
+			heading: 'Breakfast',
+			elements: ['Two eggs with toast', 'Apple with peanut butter']
+		},
+		{
+			heading: 'SOCI 101D',
+			elements: ['9:05 - 10:00']
+		},
+		{
+			heading: 'Break',
+			elements: ['Work in Aroma\'s', 'Office hours']
+		},
+		{
+			heading: 'COMP 465W',
+			elements: ['11:15 - 12:10']
+		},
+		{
+			heading: 'MATH 311',
+			elements: ['12:20 - 1:15']
+		},
+		{
+			heading: 'Lunch',
+			elements: ['Steak, vegetables, BCAA']
+		},
+		{
+			heading: 'Workout',
+			elements: ['Cardio on treadmill', 'Triceps and Chest']
+		},
+		{
+			heading: 'Post workout',
+			elements: ['Dates', 'Protein shake', 'Shower']
+		},
+		{
+			heading: 'Homework',
+			elements: ['4:00 - 6:30', 'EVL and water around 6:00']
+		},
+		{
+			heading: 'Dinner',
+			elements: ['Chicken, 2 eggs, small salad with tuna']
+		},
+		{
+			heading: 'Homework',
+			elements: ['7:30 - 10:00']
+		},
+		{
+			heading: 'Sleep',
+			elements: ['Go to sleep by 11:00', 'Wake up at 8:00']
+		}
+	],
+	[	// Tuesday
+		{
+			heading: 'Breakfast',
+			elements:['Oatmeal with apple', '2 eggs']
+		},
+		{
+			heading: 'COMP 380',
+			elements:['9:15 - 10:35']
+		},
+		{
+			heading: 'THRS 114',
+			elements:['10:45 - 12:05']
+		},
+		{
+			heading: 'Lunch',
+			elements:['Small salad with one egg']
+		},
+		{
+			heading: 'COMP 370',
+			elements:['2:30 - 3:50']
+		},
+		{
+			heading: 'Snack',
+			elements:['Yogurt with granola, BCAA']
+		},
+		{
+			heading: 'Workout',
+			elements:['4:30 - 6:00', 'Cardio on treadmill', 'Biceps and Back']
+		},
+		{
+			heading: 'Post workout',
+			elements:['Dates', 'Protein shake', 'Shower']
+		},
+		{
+			heading: 'Homework',
+			elements:['Until 8:00']
+		},
+		{
+			heading: 'Dinner',
+			elements:['Chicken, tuna, vegetables']
+		},
+		{
+			heading: 'Homework',
+			elements:['Until 10:30']
+		},
+		{
+			heading: 'Sleep',
+			elements:['Go to sleep by 11:00', 'Wake up at 8:00']
+		},
+	],
+	[	// Wednesday
+		{
+			heading: 'Breakfast',
+			elements: ['Two eggs with toast', 'Apple with peanut butter']
+		},
+		{
+			heading: 'SOCI 101D',
+			elements: ['9:05 - 10:00']
+		},
+		{
+			heading: 'Break',
+			elements: ['Work in Aroma\'s', 'Office hours']
+		},
+		{
+			heading: 'COMP 465W',
+			elements: ['11:15 - 12:10']
+		},
+		{
+			heading: 'MATH 311',
+			elements: ['12:20 - 1:15']
+		},
+		{
+			heading: 'Lunch',
+			elements: ['Steak, vegetables, BCAA']
+		},
+		{
+			heading: 'Workout',
+			elements: ['Cardio on treadmill', 'Legs and lower back']
+		},
+		{
+			heading: 'Post workout',
+			elements: ['Dates', 'Protein shake', 'Shower']
+		},
+		{
+			heading: 'Homework',
+			elements: ['4:00 - 6:30', 'EVL and water around 6:00']
+		},
+		{
+			heading: 'Dinner',
+			elements: ['Chicken, 2 eggs, small salad with tuna']
+		},
+		{
+			heading: 'Homework',
+			elements: ['7:30 - 10:00']
+		},
+		{
+			heading: 'Sleep',
+			elements: ['Go to sleep by 11:00', 'Wake up at 8:00']
+		}
+	],
+	[ // Thursday
+		{
+			heading: 'Breakfast',
+			elements:['Oatmeal with apple', '2 eggs']
+		},
+		{
+			heading: 'COMP 380',
+			elements:['9:15 - 10:35']
+		},
+		{
+			heading: 'THRS 114',
+			elements:['10:45 - 12:05']
+		},
+		{
+			heading: 'Lunch',
+			elements:['Small salad with one egg']
+		},
+		{
+			heading: 'COMP 370',
+			elements:['2:30 - 3:50']
+		},
+		{
+			heading: 'Snack',
+			elements:['Yogurt with granola, BCAA']
+		},
+		{
+			heading: 'Workout',
+			elements:['4:30 - 6:00', 'Cardio on treadmill', 'Chest - Bench']
+		},
+		{
+			heading: 'Post workout',
+			elements:['Dates', 'Protein shake', 'Shower']
+		},
+		{
+			heading: 'Homework',
+			elements:['Until 8:00']
+		},
+		{
+			heading: 'Dinner',
+			elements:['Chicken, tuna, vegetables']
+		},
+		{
+			heading: 'Homework',
+			elements:['Until 10:30']
+		},
+		{
+			heading: 'Sleep',
+			elements:['Go to sleep by 11:00', 'Wake up at 8:00']
+		},
+	],
+	[	// Friday
+		{
+			heading: 'Breakfast',
+			elements: ['Two eggs with toast', 'Apple with peanut butter']
+		},
+		{
+			heading: 'SOCI 101D',
+			elements: ['9:05 - 10:00']
+		},
+		{
+			heading: 'Break',
+			elements: ['Work in Aroma\'s', 'Office hours']
+		},
+		{
+			heading: 'COMP 465W',
+			elements: ['11:15 - 12:10']
+		},
+		{
+			heading: 'MATH 311',
+			elements: ['12:20 - 1:15']
+		},
+		{
+			heading: 'Lunch',
+			elements: ['Steak, vegetables, BCAA']
+		},
+		{
+			heading: 'Workout',
+			elements: ['Cardio on treadmill', 'Biceps and back - Calisthenics']
+		},
+		{
+			heading: 'Post workout',
+			elements: ['Dates', 'Protein shake', 'Shower']
+		},
+		{
+			heading: 'Homework',
+			elements: ['4:00 - 6:30', 'EVL and water around 6:00']
+		},
+		{
+			heading: 'Dinner',
+			elements: ['Chicken, 2 eggs, small salad with tuna']
+		},
+		{
+			heading: 'Homework',
+			elements: ['7:30 - 10:00']
+		},
+		{
+			heading: 'Sleep',
+			elements: ['Go to sleep by 11:00', 'Wake up at 8:00']
+		}
+	],
+	[	// Saturday
+		{
+			heading: 'Breakfast',
+			elements:['Scrambled eggs', 'Apple with peanut butter']
+		},
+		{
+			heading: 'Homework',
+			elements:['9:00 - 11:00', 'EVL and water around 11:30']
+		},
+		{
+			heading: 'Beach or pool',
+			elements:['12:00 - 2:00']
+		},
+		{
+			heading: 'Lunch',
+			elements:['Steak, carrots, cucumbers']
+		},
+		{
+			heading: 'Homework',
+			elements:['3:00 - 4:30']
+		},
+		{
+			heading: 'Snack',
+			elements:['Carrots, cucumbers, yogurt']
+		},
+		{
+			heading: 'Homework',
+			elements:['5:00 - 6:30', 'EVL and water around 6:00']
+		},
+		{
+			heading: 'Dinner',
+			elements:['Large salad', 'Tuna and 2 eggs']
+		},
+		{
+			heading: 'Sleep',
+			elements:['Go to sleep by 11:00', 'Wake up at 8:00']
+		}
+	]
+]
+
+var week = {0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'}
+
+$(document).ready(function() {
+	var day = (new Date()).getDay()
+
+	getSchedule(day)
+
+	$('h1').on('click', function() {
+		day = (day + 1) % 7
+		getSchedule(day)
+	})
+})
+
+function getSchedule(day) {
+	str = '<h1>' + week[day] + '</h1>'
+	$.each(schedule[day], function(index, value) {
+		str += '<h3>' + value.heading + '</h3>'
+		$.each(value.elements, function(index, value) {
+			str += '<li>' + value + '</li>'
+		})
+	})
+	$('body').html(str)
+}
